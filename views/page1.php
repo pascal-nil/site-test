19fc8aff1311acf803fde67100f75c839994d0df
pascal.benoit <pascal.benoit@acemediastools.fr>
2018-05-14 12:49:24 +0200

    test

---
+++ views/page1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr-FR" dir="auto">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Alicia</title>
+
+    <link rel="stylesheet" href="/page-test/styles/styles.css">
+
+
+
+</head>
+<body>
+<?php include 'header.php';?>
+<div class="page-container page-1">
+    je suis la page 1.
+</div>
+
+</body>
+</html>
---
+++ views/page4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr-FR" dir="auto">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Alicia</title>
+
+    <link rel="stylesheet" href="/page-test/styles/styles.css">
+
+
+
+</head>
+<body>
+<?php include 'header.php';?>
+<div class="page-container page-4">
+    je suis la page 4.
+</div>
+
+</body>
+</html>
